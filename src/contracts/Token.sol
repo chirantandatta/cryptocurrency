fe3ec8cbcb11941166e19163a95680da2a793664
Chirantan Datta <chirantandatta123@gmail.com>
2021-12-19 21:38:21 +0530

    changed

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -34,10 +34,10 @@ contract Token {
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
+          allowance[msg.sender][_spender] = _value;
+          emit Approval(msg.sender, _spender, _value);
+          return true;
+      }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= balanceOf[_from]);
